c179c580bcd3bddd7000c4a9a57a66f6e0429aa3
WalsGit <108340184+WalsGit@users.noreply.github.com>
2023-12-19 15:21:31 +0100

    updated migration

--- migrations/2023_12_05_000001_add_columns_to_tags.php
+++ migrations/2023_12_05_000001_add_columns_to_tags.php
@@ -2,6 +2,21 @@
 
 use Flarum\Database\Migration;
 
+$settings = [
+    'previewText' => 0,
+    'cardBadges' => 0,
+    'cardFooter' => 0,
+    'Views' => 0,
+    'markCards' => 0,
+    'Replies' => 0,
+    'onIndexPage' => 0,
+    'smallCards' => 10,
+    'desktopCardWidth' => 49,
+    'tabletCardWidth' => 49,
+    'allowedTags' => [],
+    'default_image_path' => null
+];
+
 return Migration::addColumns('tags', [
-    'walsgit_dc_settings' => ['text', 'nullable' => true]
+    'walsgit_dc_settings' => ['text', 'nullable' => , 'default' => json_encode($settings)]
 ]);
